d0a936e142b03897fdcb0b7051ba2b66373fdfaf
Alpana Atluri <atluria@umich.edu>
2023-10-06 18:55:34 -0500

    html/css-app

---
+++ aurora/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="rectangle" />
+        <div className="overlap-group">
+          <p className="text-wrapper">
+            Rebuilding Together Aurora is a nonprofit organization that focuses on providing critical home repairs, home
+            safety modifications, and energy efficiency upgrades to homeowners in need. Their mission is to create safe
+            and healthy housing for individuals and families who may be low-income, senior citizens, military veterans,
+            people living with disabilities, or families with young children living in unsafe conditions.
+          </p>
+        </div>
+        <div className="overlap">
+          <div className="ellipse" />
+          <img className="image" alt="Image" src="img/image-2.png" />
+        </div>
+        <div className="frame">
+          <div className="text-wrapper-2">Sign Up</div>
+        </div>
+        <div className="overlap-2">
+          <div className="frame-2" />
+          <img className="donate" alt="Donate" src="donate.png" />
+        </div>
+        <img className="img" alt="Image" src="img/image-3.png" />
+        <img className="image-2" alt="Image" src="img/image-4.png" />
+      </div>
+    </div>
+  );
+};
+export default App;
