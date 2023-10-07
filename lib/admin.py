9df3946ab0eca4582afbf4d1372d676513455ad5
jacobkieser <jkieser@cs.washington.edu>
2023-10-06 19:58:39 -0500

    Added sampleDB for better collaboration and additional admin endpoint

--- lib/admin.py
+++ lib/admin.py
@@ -5,8 +5,22 @@ import os
 app = Flask(__name__)
 
 houses = {}
+volunteers = {}
 
 @app.route("/populateDashboard")
 def populateDashboard(userid, message):
     return houses.values()
+
+@app.route("/mostRecentVolunteers/<n>")
+def getNMostRecentUsers(n):
+    recent = []
+    
+    vols = volunteers.values()
+    for i in range(n):
+        recent.append(vols[i]['firstName'] + vols[i]['lastName'])
+        
+    return recent
+
+
+
 
---
+++ lib/sampleDB.py
@@ -0,0 +1,21 @@
+
+# TO IMPORT THESE HASHMAPS, use
+# import sampleDB.py
+# or
+# from sampleDB.py import HOUSES_COLLECTION -  to get just houses hashmap.
+# or
+# from sampleDB.py import VOLUNTEERS_COLLECTION - to get just volunteers hashmap.
+
+
+HOUSES_COLLECTION = {
+    7543:{
+        "location":"Seattle, WA"
+    }
+}
+
+VOLUNTEERS_COLLECTION = {
+    8293:{
+        "firstName":"John",
+        "lastName":"Doe"
+    }
+}
