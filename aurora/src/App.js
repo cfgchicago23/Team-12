df4234e3321672f71e73636c887c31fbc5885fb3
Alpana Atluri <atluria@umich.edu>
2023-10-07 05:03:56 -0500

    Fixed homepage

--- aurora/src/App.js
+++ aurora/src/App.js
@@ -11,15 +11,6 @@ import "./App.css";
 
 import {Link} from "react-router-dom";
 
-//import {Link} from "react-router-dom";
-import {
-  BrowserRouter as Router, 
-  Routes, 
-  Route
-} from 'react-router-dom';
-import { SignUp } from './components/SignUp';
-import { HomePage } from './components/HomePage';
-//import image-2 from "~/src/img/image-2.png";
 
 //:~/src/img/image-2.png"
 
